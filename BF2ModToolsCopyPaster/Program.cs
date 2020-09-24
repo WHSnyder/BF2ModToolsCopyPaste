4fd4822df2c96ef837d402e42595aed7fe1c974e
Will Snyder <willdotsnyder@gmail.com>
2020-09-24 11:40:29 -0700

    Seed

---
+++ BF2ModToolsCopyPaster/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BF2ModToolsCopyPaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //copy
+            //get linked odfs
+
+            //get odf model dependencies
+
+            //for each model, get texture deps
+
+            //write all paths to temp file
+
+
+        //paste
+            //if odf and msh subfolders exist  
+                //for each file
+                    //check if file exists if not paste
+
+              
+        }
+    }
+}
