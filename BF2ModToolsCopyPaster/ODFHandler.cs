4c744709ceebe891c47cda06d4d19093e6b7fef7
Will Snyder <willdotsnyder@gmail.com>
2020-09-25 14:32:34 -0400

    Properly identifies all dependencies of basic ODFs and their linked MSHs

--- BF2ModToolsCopyPaster/ODFHandler.cs
+++ BF2ModToolsCopyPaster/ODFHandler.cs
@@ -1,10 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BF2ModToolsCopyPaster
 {
-    class Class1
+    class ODFHandler
     {
+        private static Regex tstRx = new Regex("^(?<property>\\w+)\\s+=\\s+\"(?<value>.*)\"\\s*$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private Dictionary<string, string> properties;
+        private string path;
+
+
+        public static ODFHandler GetODFHandler(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return null;
+            }
+
+            Dictionary<string, string> propertiesMap = new Dictionary<string, string>();
+
+            foreach (string line in System.IO.File.ReadAllLines(path))
+            {
+                //Console.WriteLine("Line: {0}", line);
+
+                foreach (Match match in tstRx.Matches(line))
+                { 
+                    GroupCollection groups = match.Groups;
+
+                    string propertyName = groups["property"].Value;
+                    string propertyValue = groups["value"].Value;
+
+                    //Console.WriteLine("\tMATCH:   Property = {0}, Value = {1}",
+                                        //propertyName, propertyValue);
+
+                    propertiesMap[propertyName] = propertyValue;
+                }
+            }
+
+            return new ODFHandler(propertiesMap, path);
+        }
+           
+
+        private ODFHandler(Dictionary<string,string> propMap, string filePath)
+        {
+            properties = propMap;
+            path = filePath;
+        }
+
+
+        public string GetPropertyValue(string propertyName)
+        {
+            return properties[propertyName];
+        }
     }
 }
--- BF2ModToolsCopyPaster/Program.cs
+++ BF2ModToolsCopyPaster/Program.cs
@@ -1,27 +1,91 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Net;
+using System.Collections.Generic;
+
+using LibSWBF2.MSH;
+using LibSWBF2.MSH.Chunks;
+
 
 namespace BF2ModToolsCopyPaster
 {
     class Program
     {
+        //private static string 
+
         static void Main(string[] args)
         {
-        //copy
+            List<string> fileDeps = new List<string>();
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please provide path(s) to ODF file...");
+                return;
+            }
+
+            string filePath = args[0];
+
+            ODFHandler handle = ODFHandler.GetODFHandler(filePath);
+
+            string geomName = handle.GetPropertyValue("GeometryName");
+            string mshFileName = geomName.Contains(".msh") ? geomName : geomName + ".msh";
+
+            DirectoryInfo di = new DirectoryInfo(Path.GetDirectoryName(Path.GetFullPath(filePath)));
+
+            string exeDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            Console.WriteLine("Exe dir: {0}", exeDir);
+
+            string worldFolderPath = di.Parent.FullName;
+            string odfFolderPath = di.FullName;
+            string mshFilePath = worldFolderPath + "\\msh\\" + mshFileName;
+
+            Console.WriteLine("ODF folder: {0}", odfFolderPath);
+            Console.WriteLine("World folder: {0}", worldFolderPath);
+
+            if (File.Exists(mshFilePath))
+            {
+                Console.WriteLine("Found msh at {0}", mshFilePath);
+                fileDeps.Add(mshFilePath);
+
+                //Find texture dependencies
+                MSH mesh = MSH.LoadFromFile(mshFilePath);
+
+                foreach(MATD mat in mesh.Materials)
+                {
+                    foreach (string texName in mat.Textures)
+                    {
+                        if (texName == null || !texName.EndsWith(".tga")) continue;
+
+                        string texturePath = Path.GetDirectoryName(mshFilePath) + "\\" + texName;
+
+                        if (!fileDeps.Contains(texturePath))
+                        {
+                            fileDeps.Add(texturePath);
+                        }
+                    }
+                }
+            }
+
+            System.IO.File.WriteAllLines("deps.txt", fileDeps.ToArray());
+
+
+            //MSH mesh = MSH.LoadFromFile();
+
+
+
+            //copy
             //get linked odfs
-
             //get odf model dependencies
-
             //for each model, get texture deps
-
             //write all paths to temp file
 
-
-        //paste
+            //paste
             //if odf and msh subfolders exist  
-                //for each file
-                    //check if file exists if not paste
+            //for each file
+            //check if file exists if not paste
 
-              
+
         }
     }
 }
